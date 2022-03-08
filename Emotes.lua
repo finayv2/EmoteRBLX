2f6b8004c37cf788b1529b5a2ac609af220ca4ab
finayv2 <75693103+finayv2@users.noreply.github.com>
2022-03-07 20:27:56 -0500

    Auto Gets emotes

--- Emotes.lua
+++ Emotes.lua
@@ -1,79 +1,30 @@
-local emoteTable = {
-    ["Shrug"] = {3576968026},
-    ["Point2"] = {3576823880},
-    ["Tilt"] = {3360692915},
-    ["Robot"] = {3576721660},
-    ["Stadium"] = {3360686498},
-    ["Salute"] = {3360689775},
-    ["Hello"] = {3576686446},
-    ["Jacks"] = {3570649048},
-    ["Around Town"] = {3576747102},
-    ["Twirl"] = {3716633898},
-    ["Monkey"] = {3716636630},
-    ["Sneaky"] = {3576754235},
-    ["Side to Side"] = {3762641826},
-    ["Greatest"] = {3762654854},
-    ["Louder"] = {3576751796},
-    ["Dizzy"] = {3934986896},
-    ["Get Out"] = {3934984583},
-    ["Fishing"] = {3994129128},
-    ["Tree"] = {4049634387},
-    ["Line Dance"] = {4049646104},
-    ["Haha"] = {4102315500},
-    ["Idol"] = {4102317848},
-    ["T"] = {3576719440},
-    ["Top Rock"] = {3570535774},
-    ["Zombie"] = {4212496830},
-    ["Dorky Dance"] = {4212499637},
-    ["Fast Hands"] = {4272351660},
-    ["Baby Dance"] = {4272484885},
-    ["Celebrate"] = {3994127840},
-    ["Fancy Feet"] = {3934988903},
-    ["Y"] = {4391211308},
-    ["Shuffle"] = {4391208058},
-    ["Shy"] = {3576717965},
-    ["Bodybuilder"] = {3994130516},
-    ["Fashionable"] = {3576745472},
-    ["Godlike"] = {3823158750},
-    ["Curtsy"] = {4646306583},
-    ["Air Dance"] = {4646302011},
-    ["Happy"] = {4849499887},
-    ["Sad"] = {4849502101},
-    ["Sleep"] = {4689362868},
-    ["Disagree"] = {4849495710},
-    ["Power Blast"] = {4849497510},
-    ["Agree"] = {4849487550},
-    ["Keeping Time"] = {4646306072},
-    ["Jumping Wave"] = {4940602656},
-    ["Confused"] = {4940592718},
-    ["Hero Landing"] = {5104377791},
-    ["Tantrum"] = {5104374556},
-    ["Cower"] = {4940597758},
-    ["Bored"] = {5230661597},
-    ["Beckon"] = {5230615437},
-    ["Swish"] = {3821527813},
-    ["High Wave"] = {5915776835},
-    ["Rock On"] = {5915782672},
-    ["Applaud"] = {5915779043},
-    ["Floss Dance"] = {5917570207},
-    ["Dolphin Dance"] = {5938365243},
-    ["Jumping Cheer"] = {5895009708},
-    ["Break Dance"] = {5915773992},
-    ["Panini Dance"] = {5915781665},
-    ["HOLIDAY Dance"] = {5938396308},
-    ["Rodeo Dance"] = {5938397555},
-    ["Old Town Road Dance"] = {5938394742},
-    ["Rock Guitar"] = {6532155086},
-    ["Rock Star"] = {6533100850},
-    ["Drum Master"] = {6531538868},
-    ["Drum Solo"] = {6532844183},
-    ["Air Guitar"] = {3696761354},
-    ["Cha-Cha"] = {3696764866},
-    ["Heisman Pose"] = {3696763549},
-    ["Hype Dance"] = {3696757129},
-    ["Superhero Reveal"] = {3696759798},
-    ["Country Line Dance"] = {5915780563};
-	  ["Quiet Waves"] = {7466046574};
-}
+local cursor = ''
+local emotesTable = {}
 
-return emoteTable
+while true do
+    local requestString = ('https://catalog.roblox.com/v1/search/items/details?Category=%s&Subcategory=%s&IncludeNotForSale=true&Limit=30&Cursor=%s'):format(
+        game:GetService('HttpService'):JSONDecode(game:HttpGet('https://catalog.roblox.com/v1/categories')).AvatarAnimations, game:GetService('HttpService'):JSONDecode(game:HttpGet('https://catalog.roblox.com/v1/subcategories')).EmoteAnimations, cursor
+    )
+    local response = game:GetService('HttpService'):JSONDecode(game:HttpGet(requestString))
+    cursor = response.nextPageCursor
+    
+    for _, data in ipairs(response.data) do
+        table.insert(emotesTable, {
+            data.name,
+            data.id
+        })
+    end
+
+    if not cursor then
+        break
+    end
+end
+
+local RobloxEmotes = {}
+local EmoteChoices = {}
+for _, emote in ipairs(emotesTable) do
+    RobloxEmotes[string.split(emote[1], "-")[1]] = {emote[2]}
+    table.insert(EmoteChoices, string.split(emote[1], "-")[1])
+end
+
+return RobloxEmotes, EmoteChoices
